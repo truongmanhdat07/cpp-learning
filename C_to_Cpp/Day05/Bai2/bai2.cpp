0fe212aadb67b8ad5b93bed6d17a39554f6f18a4
Trương Mạnh Đạt <truongmanhdat061@gmail.com>
2026-07-27 04:46:20 +0700

    Add classes for phapbao and kiemkhi with memory management

--- C_to_Cpp/Day05/Bai2/bai2.cpp
+++ C_to_Cpp/Day05/Bai2/bai2.cpp
@@ -1,1 +1,34 @@
 #include<iostream>
+#include<string>
+
+using namespace std;
+
+class phapbao{
+		public :
+			phapbao() {
+				cout << "\nPhap bao da duoc them vao";
+			}
+			virtual ~phapbao(){
+				cout << "\nPhap bao da bi huy di";
+			}
+};
+class kiemkhi : public phapbao{
+	private :
+		int *kiemtran;
+	public :
+		kiemkhi(){
+			kiemtran= new int [10];
+			cout << "\nKiem tran gom 10 kiem khi vua duoc them vao";
+		}
+		~kiemkhi() override{
+			cout << "\nKiem tran da bi huy di";
+			delete[] kiemtran;
+		}
+};
+
+int main(){
+	phapbao *a= new kiemkhi();
+	delete a;
+	
+	return 0;
+}
