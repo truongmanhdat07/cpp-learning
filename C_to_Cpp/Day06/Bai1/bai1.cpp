7a078ddce75a7ac9a4c66077147c672b877a3eba
Trương Mạnh Đạt <truongmanhdat061@gmail.com>
2026-07-30 19:25:32 +0700

    Implement student classes with grading methods
    Added classes for student types and their methods.

--- C_to_Cpp/Day06/Bai1/bai1.cpp
+++ C_to_Cpp/Day06/Bai1/bai1.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-#incluce<string>
+#include<string>
 
 using namespace std;
 
@@ -20,3 +20,51 @@ class sinhvien{
 			cout << "\nDa don dep bo nho sinh vien " << ten ;
 		}
 };
+class sinhvienit : public sinhvien{
+	private:
+		float diemcode;
+		float diemtoan;
+	public:
+		sinhvienit(string t,string msv,float code,float toan) : sinhvien(t,msv){
+			diemcode=code;
+			diemtoan=toan;
+		}
+		float tinhdiem(){
+			return (diemcode+diemtoan)/2;
+		}
+		void inthongtin(){
+			cout << "\nSinh vien :" << ten << " co ma sinh vien " << masv << " co diem tong ket la :" << tinhdiem();
+		}
+};
+class sinhvienngonngu : public sinhvien{
+	private:
+		float ielts;
+	public:	
+		sinhvienngonngu(string t,string msv,float ie):sinhvien(t,msv){
+			ielts=ie;
+		}
+		float tinhdiem(){
+			if(ielts>=7)			return 10;
+			else if(ielts >=6.0)	return 8.5;
+			else					return 7.0;
+		}
+		void inthongtin(){
+			cout << "\nSinh vien :" << ten << " co ma sinh vien " << masv << " co diem tong ket la :" << tinhdiem();
+		}
+};
+
+int main(){
+	sinhvien *k66[2];
+	k66[0]= new sinhvienit("Hoang","H001",7,8);
+	k66[1]= new sinhvienngonngu("Linh","H002",8.0);
+	
+	for(int i=0;i<2;i++){
+		k66[i] -> inthongtin();
+	}
+	
+	for(int i=0;i<2;i++){
+		delete k66[i];
+	}
+	
+	return 0;
+}
